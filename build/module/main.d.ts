e0297db6b5249c398948fd7433d236c48e133b07
dandelany <dan.delany@gmail.com>
2019-10-11 02:59:38 -0700

    allow genre to be undefined

--- build/module/main.d.ts
+++ build/module/main.d.ts
@@ -1,3 +1,3 @@
 export declare const x = 4;
 export declare function randElem<T>(list: T[]): T;
-export declare function generate(genre: string): string;
+export declare function generate(genre: string | undefined): string;
